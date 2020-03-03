0633b4d02ca062cc2152a6ceedd5f63c1ff937b1
veereshbahri <veereshbahri@gmail.com>
2020-03-03 01:34:56 -0600

    DEmo for maven

---
+++ dem01/src/main/java/com/abc/Dao.java
@@ -0,0 +1,15 @@
+package com.abc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dao {
+	public List<String> showdata()
+	{
+		List<String> list=new ArrayList<String>();
+		list.add("happy");
+		list.add("bday");
+		return list;
+	}
+
+}
---
+++ dem01/src/main/java/com/abc/Serv.java
@@ -0,0 +1,41 @@
+package com.abc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Serv
+ */
+public class Serv extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Serv() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ");
+		Dao d=new Dao();
+		List<String> list=d.showdata();
+		PrintWriter out = response.getWriter();
+		out.print(list);
+		out.close();
+	}
+
+	
+
+}
